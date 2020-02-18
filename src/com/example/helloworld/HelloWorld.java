6980a3e3c4c4d3f0c968cd60c1be75ce761ecf4c
Irina Gavrina <igavrina@ozon.ru>
2020-02-18 19:55:15 +0300

    Input file parsing - done

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,8 +3,8 @@ package com.example.helloworld;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -13,24 +13,27 @@ public class HelloWorld {
 
     private static void readInputFile(String filePath) {
         System.out.println("Reading file...");
-        List<Integer> list = null;
-        String content = "";
-        List<List<Integer>> listOfLists = null;
+        List<List<Integer>> listOfLists = new ArrayList<>();
         try {
-            //content = Files.lines(Paths.get(filePath)).collect(Collectors.joining());
             Files.lines(Paths.get(filePath))
                     .forEach( line ->
-                            // ToDo распарсить строку чисел через пробелы
-                            System.out.println(Integer.parseInt(line))
+                            listOfLists.add(parseString(line))
                     );
-//            list = Files.lines(Paths.get(filePath))
-//                    .map(Integer::parseInt)
-//                    .collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println("Printing result");
-        //System.out.println(list);
+
+        System.out.println(listOfLists);
+    }
+
+    private static List<Integer> parseString(String str) {
+        String[] strMas= str.split("[ ]"); //получили массив без пробелов
+        List<Integer> result = new ArrayList<>(strMas.length);
+        for (int i = 0; i < strMas.length; i++) {
+            result.add(Integer.parseInt(strMas[i]));
+        }
+        return result;
     }
 }
