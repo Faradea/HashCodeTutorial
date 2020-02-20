367059d536f9e9f09624d0e82e07bf1ff327ee91
Valentin <verbovoy@mail.ru>
2020-02-20 22:37:10 +0300

    Хороший пример работы со строками.

--- src/com/example/helloworld/Calculation.java
+++ src/com/example/helloworld/Calculation.java
@@ -12,17 +12,16 @@ public class Calculation {
         int daysLeft=0;
 
         List<Integer> processedArray = new ArrayList<Integer>();
-        processedArray.add(2);
+        /*processedArray.add(2);
         processedArray.add(1);
         processedArray.add(0);
-        processedArray.remove(2);
+        processedArray.remove(0);*/
         System.out.println(processedArray);
 
         //processLib(processedArray);
     }
-    public static int processLib(List<Integer> processedArray)
+    public static int processLib(List<Integer> processedArray,int daysLeft)
     {
-        int daysLeft=0;
         for(int i=0; i < librariesList.size(); i++) {
             if(processedArray.indexOf(i)>=0)
                 continue;
@@ -31,10 +30,9 @@ public class Calculation {
             //int daysToProcess=librariesList.get(i).booksCount/librariesList.get(i).booksPerDay;
             //int devider=(librariesList.get(i).booksCount%librariesList.get(i).booksPerDay>0?1:0);
             ///List<SomeBean> newList = new ArrayList<SomeBean>(otherList);
-            processLib(processedArray);
-            processedArray.remove(i);
-
+            daysLeft = processLib(processedArray,daysLeft);
+            processedArray.remove(processedArray.size()-1);
         }
-        return 0;
+        return daysLeft;
     }
 }
